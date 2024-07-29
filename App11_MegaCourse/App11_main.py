88427ac158a3f2f89e21e550a0ad1238fa147883
hadi.khadim <abhadi027@gmail.com>
2024-07-29 17:04:12 +0500

    commit

---
+++ App11_MegaCourse/App11_main.py
@@ -0,0 +1,22 @@
+# #Hotel Booking App
+# user can see a list of Hotel
+# user can booka Hotel
+# user can get a reservation ticket
+
+
+# classes:
+# user 
+# Hotel
+# reservationticket
+import pandas as pd
+
+df = pd.read_csv('hotel.csv')
+class Hotel:
+    def book(self):
+        pass
+
+class ReservationTicket:
+    def generate(self):
+        pass
+
+print(df)
