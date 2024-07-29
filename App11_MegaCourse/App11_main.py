6be4ece6b1d52ae65d8c6d24cdbb20e3d6373884
hadi.khadim <abhadi027@gmail.com>
2024-07-29 20:22:32 +0500

    App11_half

--- App11_MegaCourse/App11_main.py
+++ App11_MegaCourse/App11_main.py
@@ -1,22 +1,67 @@
-# #Hotel Booking App
-# user can see a list of Hotel
-# user can booka Hotel
-# user can get a reservation ticket
-
-
-# classes:
-# user 
-# Hotel
-# reservationticket
 import pandas as pd
 
-df = pd.read_csv('hotel.csv')
+# Read the CSV files
+df = pd.read_csv('hotels.csv', dtype={'id': str})
+df2 = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
+
+
 class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df['id'] == self.hotel_id, 'name'].squeeze()
+
     def book(self):
-        pass
+        '''Book a hotel by changing the availability to No'''
+        df.loc[df['id'] == self.hotel_id, 'available'] = 'no'
+        df.to_csv('hotels.csv', index=False)
+
+    def available(self):
+        availability = df.loc[df['id'] == self.hotel_id, 'available'].squeeze()
+        return availability == 'yes'
+
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
     def generate(self):
-        pass
+        content = f'''
+        Thank you for your reservation
+        Here are your booking data
+        Name: {self.customer_name}
+        Hotel Name: {self.hotel.name}
+        '''
+        return content
+
+
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {
+            'number': self.number,
+            'expiration': expiration,
+            'holder': holder,
+            'cvc': cvc
+        }
+        if card_data in df2:
+            return True
+
 
 print(df)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
+
+if hotel.available():
+    credit_card = CreditCard(number='1234567890123456')
+    if credit_card.validate(expiration='12/26', holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with your card")
+else:
+    print('Sorry! Hotel is full')
